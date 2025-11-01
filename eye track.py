741b084f8af538bbdac595f07ef1eb1bc310c9c1
Shakhoyat <shakoyatsujon@gmail.com>
2025-11-01 09:35:54 +0600

    Update eyelid servo range for improved control and accuracy

--- eye track.py
+++ eye track.py
@@ -30,8 +30,8 @@ def map_range(value, in_min, in_max, out_min, out_max):
 
 # --- Configurable Ranges ---
 # Eyelid servo range
-servo_left_lid_min = 50 # eyelid final
-servo_left_lid_max = 135
+servo_left_lid_min = 70 # eyelid final
+servo_left_lid_max = 110
 lid_gap_min = 3
 lid_gap_max = 11
 
