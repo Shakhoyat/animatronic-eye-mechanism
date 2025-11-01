fd0bc92eebe220a0594085daeda804da25789b81
Shakhoyat <shakoyatsujon@gmail.com>
2025-11-01 10:16:23 +0600

    Add baseline difference handling for eye horizontal movement

--- eye track.py
+++ eye track.py
@@ -243,7 +243,16 @@ while cap.isOpened():
             # S1 & S2: Eye horizontal movement (35-155 degree range)
             s1_eye_left_lr = int(left_eye_horizontal_angle)
             s2_eye_right_lr = int(right_eye_horizontal_angle)
-            
+            if(baseline_diff < -5):
+                s1_eye_left_lr = -50
+                s2_eye_right_lr = 50
+            elif(baseline_diff > 5):
+                s1_eye_left_lr = 50
+                s2_eye_right_lr = -50
+            else:
+                s1_eye_left_lr = 0
+                s2_eye_right_lr = 0
+
             # S3 & S4: Eyelids
             s3_eyelid_left = int(left_lid_mapped)
             s4_eyelid_right = int(right_lid_mapped)
